8bc5992db637a63387fe249e1839e1bb1122e5fc
MShahryiar <muhammadshahryiar@yahoo.com>
2022-05-22 14:09:51 +0500

    Figured it out. I was using {} in useState, instead of []. Because it was only the list of ingredients and not an array containing other lists, etc.  Maybe that's the reason and Its working now.

--- src/Pages/Recipe.jsx
+++ src/Pages/Recipe.jsx
@@ -5,23 +5,26 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import {useState, useEffect} from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+
 
 export default function Recipe(){
 
     let params = useParams();
 
-    const [detail, setDetail] = useState({});
+    const [detail, setDetail] = useState([]);
     const [recipeName, setRecipeName] = useState('');
+    const [ingredients, setIngredients] = useState([]);
 
     const getRecipe = async () => {
         const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=9a76d0f86c974882ba9ce8d4d782c5ef`)
         const detailData = await data.json()
         setDetail(detailData);
-        setRecipeName(detailData.title)
-       
+        setRecipeName(detailData.title);
+        setIngredients(detailData.extendedIngredients)
+        // console.log(detailData);  
 
     }
-
     useEffect(()=>{
         getRecipe();
     },[params.name])
@@ -44,11 +47,24 @@ export default function Recipe(){
                             </Row>
                         
                         </Tab>
-                        <Tab eventKey="ingredients" title="Ingredients">
-                            <h2>Ingredients</h2>
-                            {/* {detail.extendedIngredients.map((ingredient)=>(
-                        <li key={ingredient.id}>{ingredient.original}</li>
-                             ))} */}
+
+                        <Tab eventKey="ingredients"  title="Ingredients">               
+                            
+
+                             {/* <button onClick={getIngredients}>Get Ingredients</button>  */}
+                       
+                            <ListGroup className="mt-5">
+                                {ingredients.map((ingredient) =>{
+                                    return(
+                                        <ListGroup.Item key={ingredient.id}>{ingredient.original}</ListGroup.Item>
+                                    );
+                                    })}                         
+                               
+                            </ListGroup>                            
+                                
+                            
+                            
+
                         </Tab>
                     </Tabs>
                    
@@ -58,3 +74,10 @@ export default function Recipe(){
         
     );
 }
+
+{/* {detail.extendedIngredients.map((ingredient)=>(
+                        <li key={ingredient.id}>{ingredient.original}</li>
+                             ))} */}
+                             
+                             {/* Works for non array items  */}
+                             {/* <p>{detail.pricePerServing}</p> */}
